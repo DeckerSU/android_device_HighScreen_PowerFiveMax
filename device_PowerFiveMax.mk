d35d09d59f084e3a698f952c7c25b917c278f091
DeckerSU <support@decker.su>
2017-03-06 01:29:35 +0300

    fixes and additions compilation [#2]
    - added core_64_bit.mk to device.mk for support 64-bit Zygote
    - added many proprietary blobs to extract-files list
    - fixed partitions mounts in fstab
    - many device related changes in init.mt6755.rc and *usb.rc
    - added needed rc scripts, included trustonic
    - changed ueventd.mt6755.rc

--- device_PowerFiveMax.mk
+++ device_PowerFiveMax.mk
@@ -1,4 +1,5 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # These additionals go to /default.prop
