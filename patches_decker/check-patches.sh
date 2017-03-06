5f471a6324dab89c10c496be4c1c3f833333b858
DeckerSU <support@decker.su>
2017-03-06 22:57:29 +0300

    downgrading tree CM14 -> CM13 [step #1]

--- patches_decker/check-patches.sh
+++ patches_decker/check-patches.sh
@@ -1,25 +1,18 @@
 #!/bin/bash
 cd ../../../..
 cd system/core
-git apply -v --check ../../device/HighScreen/PowerFiveMax/patches_decker/0001-Remove-CAP_SYS_NICE-from-surfaceflinger.patch
-git apply -v --check ../../device/HighScreen/PowerFiveMax/patches_decker/0004-libnetutils-add-MTK-bits-ifc_ccmni_md_cfg.patch
-git apply -v --check ../../device/HighScreen/PowerFiveMax/patches_decker/0012-PATCH-xen0n-some-MTK-services-e.g.-ril-daemon-mtk-re.patch
-cd ../..
-cd bionic
-git apply -v --check ../device/HighScreen/PowerFiveMax/patches_decker/0002-Apply-LIBC-version-to-__pthread_gettid.patch
-cd ..
-cd system/sepolicy
-git apply -v --check ../../device/HighScreen/PowerFiveMax/patches_decker/0003-Revert-back-to-policy-version-29.patch
+git apply -v --check ../../device/HighScreen/PowerFiveMax/patches_decker/0001-libnetutils-add-MTK-bits.patch
+git apply -v --check ../../device/HighScreen/PowerFiveMax/patches_decker/0002-xen0n-some-MTK-services-e.g.-ril-daemon-mtk-require-.patch
 cd ../..
 cd packages/apps/Settings
-git apply -v --check ../../../device/HighScreen/PowerFiveMax/patches_decker/0005-add-author-info-in-device-info.patch
+git apply -v --check ../../../device/HighScreen/PowerFiveMax/patches_decker/0003-add-author-info-in-device-info.patch
 cd ../../..
 cd frameworks/av
-git apply -v --check ../../device/HighScreen/PowerFiveMax/patches_decker/0006-fix-access-wvm-to-ReadOptions.patch
-git apply -v --check ../../device/HighScreen/PowerFiveMax/patches_decker/0007-Disable-usage-of-get_capture_position.patch
-git apply -v --check ../../device/HighScreen/PowerFiveMax/patches_decker/0008-Partial-Revert-Camera1-API-Support-SW-encoders-for-n.patch
-git apply -v --check ../../device/HighScreen/PowerFiveMax/patches_decker/0009-add-mtk-color-format-support.patch
+git apply -v --check ../../device/HighScreen/PowerFiveMax/patches_decker/0006-Implement-mtk-color-format-support-fire855.patch
 cd ../..
-cd system/netd
-git apply -v --check ../../device/HighScreen/PowerFiveMax/patches_decker/0010-wifi-tethering-fix.patch
-cd ../..
+cd packages/apps/FMRadio/jni/fmr/ 
+git apply -v --check ../../../../../device/HighScreen/PowerFiveMax/patches_decker/0007-fix-fm-radio-power-up-mt6737m-mt6627-chip.patch
+cd ../../../../..
+cd system/netd/server/
+git apply -v --check ../../../device/HighScreen/PowerFiveMax/patches_decker/0008-WiFi-AP-fix.patch
+cd ../../..
